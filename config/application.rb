d637b1f458a11ce6364c339337c7520a50c8fb68
RogelioQuintanaRomero <rogelio.quintana@outlook.com>
2016-04-20 11:26:04 -0500

    Archivo conf env prod

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module NewsomeblogPost
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     #parametro agregado para compilar tipo de archivos señalados
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.js)
+    #config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.js)
   end
 end
